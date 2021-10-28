e502c581bc4267ba0cc11432a19c6e1c247c85a7
Opilane TTHK <opilane@tthk.ee>
2021-10-28 09:10:48 +0300

    FileSystem

---
+++ Lists/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> myWishes = new List<string>();
+            myWishes.Add("new crocs");
+            myWishes.Add("new socks");
+            myWishes.Add("new shoes");
+            myWishes.Add("new adidas sandals");
+
+            //Console.WriteLine($"There are {myWishes.Count} items on your wishlist.");
+
+            //Console.WriteLine(myWishes[0]);
+            //Console.WriteLine(myWishes[myWishes.Count - 1]);
+
+            foreach(string wish in myWishes)
+            {
+                Console.WriteLine($"Item on your wish list: {wish}");
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Enter a wish");
+            string userWish = Console.ReadLine();
+
+            myWishes.Add(userWish);
+
+            foreach(string wish in myWishes)
+            {
+                Console.WriteLine($"Item on your wish list {wish}");
+            }
+        }
+    }
+}
